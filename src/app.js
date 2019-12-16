6f2a9673d1e4f16a283498e7d86b73c66ca2db61
Bigsiski <vitaliytsisaruk@gmail.com>
2019-12-16 22:04:27 +0200

    with shorted adress

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/public', express.static('public'));
 
-app.get('/index', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join('C:/Users/Vitaliy/Desktop/WebstormProjects/server/client/public/index.html'));
 });
 app.get('/bundle.js', function(req,res){
