9b90e82cf14332e817d8f9ba0ccc1d0c89cd8b1a
Bigsiski <vitaliytsisaruk@gmail.com>
2019-12-16 17:20:53 +0200

    server sends index.html

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+import path from 'path';
 
 import {setUpConnection} from "./database/database";
 import collections from './routes/collections';
 import models from './routes/models';
+
 const app = express();
 
-app.use(cors('localhost:3000'));
+//app.use(cors('localhost:3000'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/public', express.static('public'));
+
+app.get('/index', function (req, res) {
+  res.sendFile(path.join('C:/Users/Vitaliy/Desktop/WebstormProjects/server/client/public/index.html'));
+});
+app.get('/bundle.js', function(req,res){
+  res.sendFile(path.join('C:/Users/Vitaliy/Desktop/WebstormProjects/server/client/build/bundle.js'));
+});
+app.get('/manifest.json', function(req,res){
+  res.sendFile(path.join('C:/Users/Vitaliy/Desktop/WebstormProjects/server/client/public/manifest.json'));
+});
+app.get('/style', function(req,res){
+  res.sendFile(path.join('C:/Users/Vitaliy/Desktop/WebstormProjects/server/client/build/style.css'));
+});
 
 app.use('/api/models', models);
 app.use('/api/collections', collections);
@@ -17,15 +34,12 @@ app.use('/api/collections', collections);
 //app.use('/api/login', auth);
 //app.use('/api/users', list);
 
-async function start(){
+async function start() {
   await setUpConnection();
   app.listen(5000, () => console.log("started at 5000"));
 }
+
 start();
-
-
-
-
 
 module.exports = app;
 
